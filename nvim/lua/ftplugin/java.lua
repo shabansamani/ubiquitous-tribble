7c5f9af16775823528699f27a2f8b85d3c575afe
Shaban Samani <shabansamani@gmail.com>
2024-03-22 00:18:22 -0400

    Using nvim-jdtls over jdtls

---
+++ nvim/lua/ftplugin/java.lua
@@ -0,0 +1,115 @@
+local jdtls_dir = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+local config_dir = jdtls_dir .. "/config_mac_arm"
+local plugins_dir = jdtls_dir .. "/plugins/"
+local path_to_jar = plugins_dir .. "org.eclipse.equinox.launcher_1.6.700.v20231214-2017.jar"
+local path_to_lombok = jdtls_dir .. "/lombok.jar"
+
+local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+local root_dir = require("jdtls.setup").find_root(root_markers)
+if root_dir == "" then
+  return
+end
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = vim.fn.stdpath("data") .. "/site/java/workspace-root/" .. project_name
+os.execute("mkdir " .. workspace_dir)
+
+local config = {
+  cmd = {
+    "/Users/shabansamani/.sdkman/candidates/java/current/bin/java",
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-javaagent:" .. path_to_lombok("-Xms1g"),
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+
+    "-jar",
+    path_to_jar,
+    "-configuration",
+    config_dir,
+    "-data",
+    workspace_dir,
+  },
+
+  root_dir = root_dir,
+
+  settings = {
+    java = {
+      home = "/Users/shabansamani/.sdkman/candidates/java/current",
+      eclipse = {
+        downloadSources = true,
+      },
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        runtimes = {
+          {
+            name = "JavaSE-17",
+            path = "/Users/shabansamani/.sdkman/candidates/java/current",
+          },
+        },
+      },
+      maven = {
+        downloadSources = true,
+      },
+      implementationsCodeLens = {
+        enabled = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      format = {
+        enabled = true,
+        settings = {
+          url = vim.fn.stdpath("config") .. "/lang-servers/intellij-java-google-style.xml",
+          profile = "GoogleStyle",
+        },
+      },
+    },
+    signatureHelp = { enabled = true },
+    completion = {
+      favoriteStaticMembers = {
+        "org.hamcrest.MatcherAssert.assertThat",
+        "org.hamcrest.Matchers.*",
+        "org.hamcrest.CoreMatchers.*",
+        "java.util.Objects.requireNonNull",
+        "java.util.Objects.requireNonNullElse",
+        "org.mockito.Mockito.*",
+      },
+      importOrder = {
+        "java",
+        "javax",
+        "com",
+        "org",
+      },
+    },
+    sources = {
+      organizeImports = {
+        starThreshold = 9999,
+        staticStarThreshold = 9999,
+      },
+    },
+    codeGeneration = {
+      toString = {
+        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+      },
+      useBlocks = true,
+    },
+    flags = {
+      allow_incremental_sync = true,
+    },
+    init_options = {
+      bundles = {},
+    },
+  },
+}
+
+require("jdtls").start_or_attach(config)
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -3,47 +3,48 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "jdtls", "jsonls", "html", "cssls", "jedi_language_server" }
+        ensure_installed = { "lua_ls", "tsserver", "jsonls", "html", "cssls", "jedi_language_server" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "mfussenegger/nvim-jdtls" },
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.jdtls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.jsonls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.html.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.cssls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.jedi_language_server.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
-    end
-  }
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+    end,
+  },
+  {
+    "mfussenegger/nvim-jdtls",
+  },
 }
